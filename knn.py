451532c5053004141203c4242205ad7ab9cc6721
wenzhi911225 <865530636@qq.com>
2016-01-22 12:57:39 +0800

    frist

---
+++ knn.py
@@ -0,0 +1,5 @@
+def classifyPerson():
+    resultList = ['not at all', 'in small doses', 'in large doses']
+    percentTats = float(
+        raw_input("percentage of time spent playing video games?"))
+    ffMiles = float(raw_input("frequent flier miles earned per year?"))
